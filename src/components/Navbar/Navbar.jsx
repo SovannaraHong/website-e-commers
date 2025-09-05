65729a7c1d6af1e5a9c370d91dd8e678424916d8
Hong Sovannara <rapewpew372@gmail.com>
2025-09-05 13:48:31 +0700

    navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+import logo from "../../assets/logo/logos.png";
+const Navbar = () => {
+  const navLink = [
+    { label: "home", path: "/" },
+    { label: "feature", path: "feature" },
+    { label: "Phka", path: "phka" },
+    { label: "shop", path: "shop" },
+    { label: "Blog", path: "blog" },
+    { label: "Be Our Ambastadar", path: "Outlet" },
+    { label: "page", path: "page" },
+  ];
+  const contact = [
+    {
+      icon: <i className="bx bx-user-circle"></i>,
+    },
+
+    {
+      icon: <i className="bx bx-shopping-bag"></i>,
+    },
+  ];
+  const optionLanguege = [
+    {
+      label: "English",
+      flag: "https://img.freepik.com/free-vector/illustration-uk-flag_53876-18166.jpg?semt=ais_hybrid&w=740&q=80",
+    },
+    {
+      label: "Cambodia",
+      flag: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Flag_of_Cambodia.svg/1200px-Flag_of_Cambodia.svg.png",
+    },
+  ];
+
+  const money = [{ Pon: "USA" }, { Pon: "KHMER" }];
+  const product = [{ Pon: "KOREA" }, { Pon: "KHMER" }];
+
+  return (
+    <>
+      <div className="bg-[#f5f5f5] font-popinse h-[140px] fixed top-0 left-0 right-0 shadow-lg z-[9999]">
+        <nav className="fixed flex  top-0 left-0 right-0 justify-between items-center z-[9999] px-[90px] bg-[#fdecec] font-popinse h-[80px]  ">
+          <section>
+            {/* right */}
+
+            <div className="flex justify-center items-center">
+              <div>
+                {/* <img className="w-[20px]" src={selecFlag.flag} alt="" /> */}
+              </div>
+              {/* <div>
+                <select
+                  name=""
+                  id=""
+                  onClick={(e) => {
+                    const lang = optionLanguege.find(
+                      (langs) => langs.label === e.target.value
+                    );
+                    setSelectFlag(lang);
+                  }}
+                >
+                  {optionLanguege.map((op) => (
+                    <option key={op.label}>{op.label}</option>
+                  ))}
+                </select>
+                <select>
+                  {money.map((mo, ind) => (
+                    <option key={ind}>{mo.Pon}</option>
+                  ))}
+                </select>
+                <select name="" id="">
+                  {product.map((mo, ind) => (
+                    <option key={ind}>{mo.Pon}</option>
+                  ))}
+                </select>
+              </div> */}
+            </div>
+          </section>
+          {/* center */}
+          <div className=" flex justify-center items-center font-NT">
+            <img className="w-[300px] pt-[20px]" src={logo} alt="" />
+          </div>
+
+          {/* left  */}
+          <section className="flex  justify-center items-center gap-2">
+            <div>
+              <input
+                type="text"
+                name=""
+                placeholder="Search..."
+                className="bg-[#f075aa] px-[5px] w-[140px]  cursor-pointer text-[15px] py-[5px] rounded-[5px]"
+                id=""
+              />
+              <i className="bx bx-search absolute right-[165px] top-[33px]"></i>
+            </div>
+            <div className="flex justify-center items-center gap-2">
+              {contact.map((ct, ind) => (
+                <span className="text-[25px]" key={ind}>
+                  {ct.icon}
+                </span>
+              ))}
+            </div>
+          </section>
+        </nav>
+
+        <main className="flex justify-center items-center pt-[101px] fixed top-0 left-0   right-0 z-[99]">
+          <ul className="flex justify-center items-center  gap-15 cursor-pointer uppercase list-style font-semibold font-popinse text-[14px]">
+            {navLink.map((nav, ind) => (
+              <li
+                key={ind}
+                className={`cursor-pointer transition-all duration-300 ease-in-out 
+    ${
+      location.pathname === nav.path || location.pathname === `/${nav.path}`
+        ? "text-[#fd84cf] style-list "
+        : ""
+    }`}
+              >
+                {nav.label}
+              </li>
+            ))}
+          </ul>
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/data/data.js
@@ -0,0 +1,245 @@
+export const categoryList = [
+  {
+    id: 1,
+    title: "ផ្កា​ កុលាប",
+    productName: "ផ្កា​ កុលាប",
+    images: [
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1748579763785-Kolab.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1747972511341-1.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1747972511332-2.jpg&w=128&q=75",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1747972510784-3.JPG&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1747972511279-4.JPG&w=640&q=100",
+      },
+    ],
+    price: 10,
+    discount: 10,
+    rating: 4.5,
+    topSell: true,
+  },
+  {
+    id: 2,
+    title: "Phka Chouk",
+    productName: "ផ្កា​ ឈូក",
+    images: [
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1748579773026-Phka%2520Chouk.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1747972610672-1.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1747972610663-2.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1747972610581-3.png&w=128&q=75",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1747972610655-4.png&w=640&q=100",
+      },
+    ],
+    price: 10,
+    discount: 5,
+    rating: 3.8,
+    topSell: false,
+  },
+  {
+    id: 3,
+    title: "Phkorlorn",
+    productName: "ផ្កា​ ផ្គរលាន់",
+    images: [
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1748579781429-Phkorlorn.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1748579817735-Romdoul.jpg&w=384&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1734262798056-asset-1.png&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1734262797743-asset-2.png&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1734262798178-asset-3.png&w=640&q=100",
+      },
+    ],
+    price: 10,
+    discount: 15,
+    rating: 5,
+    topSell: true,
+  },
+  {
+    id: 4,
+    title: "Romdoul",
+    productName: "ផ្កា​ រំដួល",
+    images: [
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1748579817735-Romdoul.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1747972666236-1.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1747972666227-2.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1747972665644-3.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1747972666218-4.png&w=640&q=100",
+      },
+    ],
+    price: 10,
+    discount: 0,
+    rating: 4.2,
+    topSell: false,
+  },
+  {
+    id: 5,
+    title: "Phka Nokoreach",
+    productName: "ផ្កា​ នរគរាជ",
+    images: [
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1748579790906-Phka%2520Nokoreach.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1748579790906-Phka%2520Nokoreach.jpg&w=384&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1743514707575-RE-EDIT-PKA%2528final%2529-27.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1743514706959-RE-EDIT-PKA%2528final%2529-02.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1743514707747-DSC03976.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1743514706907-RE-EDIT-PKA%2528final%2529-16.jpg&w=640&q=100",
+      },
+    ],
+    price: 10,
+    discount: 20,
+    rating: 4.7,
+    topSell: true,
+  },
+  {
+    id: 6,
+    title: "Phka Toek Dos",
+    productName: "ផ្កា​ ទឹកដោះ",
+    images: [
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1748579799489-Phka%2520Toek%2520dos.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1748579799489-Phka%2520Toek%2520dos.jpg&w=384&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1743515026028-RE-EDIT-PKA%2528final%2529-30.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1743515025367-RE-EDIT-PKA%2528final%2529-18.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1743515026047-DSC03944.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1743515025452-RE-EDIT-PKA%2528final%2529-31.jpg&w=640&q=100",
+      },
+    ],
+    price: 10,
+    discount: 8,
+    rating: 3.5,
+    topSell: false,
+  },
+  {
+    id: 7,
+    title: "Phka Angkea Dey",
+    productName: "អង្គារដី",
+    images: [
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1748579807567-Phka%2520Angkea%2520Dey.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1748579807567-Phka%2520Angkea%2520Dey.jpg&w=384&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1743515403267-RE-EDIT-PKA%2528final%2529-24.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1743515403237-RE-EDIT-PKA%2528final%2529-19.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1743515403290-DSC03910.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1743515403237-RE-EDIT-PKA%2528final%2529-13.jpg&w=640&q=100",
+      },
+    ],
+    price: 10,
+    discount: 12,
+    rating: 4.9,
+    topSell: true,
+  },
+  {
+    id: 8,
+    title: "Phka Angkea Dey",
+    productName: "អង្គារដី",
+    images: [
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1746377499639-1742614772374-1000022455.png&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1733901383866-11.png&w=1080&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1733901384099-pich%2520pisey%2520-21.jpg&w=1080&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1733901384071-13.png&w=1080&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1733901384126-pich%2520pisey%2520-15.jpg&w=1080&q=100",
+      },
+    ],
+    price: 10,
+    discount: 12,
+    rating: 4.9,
+    topSell: true,
+  },
+  {
+    id: 9,
+    title: "Bikini Cream",
+    productName: "Bikini Cream",
+    images: [
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1746377511634-1742614861763-1000022456.jpg&w=640&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1733901029705-IMG_5438.JPG&w=1080&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1733901076721-BIKINI-03.jpg&w=1080&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1733901077054-14.png&w=1080&q=100",
+      },
+      {
+        img: "https://pichpisey.shop/_next/image?url=https%3A%2F%2Fpichpiseyshop.sgp1.digitaloceanspaces.com%2F1733901077087-10.png&w=1080&q=100",
+      },
+    ],
+    price: 10,
+    discount: 12,
+    rating: 4.9,
+    topSell: true,
+  },
+];

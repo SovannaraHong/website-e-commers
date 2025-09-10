f3e4b8b854845c165fe73b9ead88dbe5567de446
Hong Sovannara <rapewpew372@gmail.com>
2025-09-10 16:21:44 +0700

    Searching

--- src/components/FeatureComponent/WrapperProduct.jsx
+++ src/components/FeatureComponent/WrapperProduct.jsx
@@ -26,27 +26,50 @@ const WrapperProduct = () => {
       </div>
     );
   }
+  const [Searching, setSearching] = useState("");
 
+  const handleSearch = (e) => {
+    setSearching(e.target.value);
+  };
+
+  const finalSearch = filterCategory.filter(
+    (p) =>
+      p.category?.name.toLowerCase().includes(Searching.toLowerCase()) ||
+      p.title.toLowerCase().includes(Searching.toLowerCase())
+  );
   return (
-    <div className="pt-[140px]">
-      <div className="py-[50px]">
-        <div className="flex justify-center items-center gap-5 font-popinse fixed top-35 left-0 right-0 z-[999] py-[25px] shadow-lg bg-[#f5f5f5]">
-          {categoryProduct.map((cate) => (
-            <button
-              key={cate.value}
-              onClick={() => setActiveCategory(cate.value)}
-              className={` text-white px-[13px] py-[5px] rounded-[10px] cursor-pointer ${
-                activeCategory === cate.value ? "bg-pink-300" : "bg-green-300"
-              }`}
-            >
-              {cate.label}
-            </button>
-          ))}
+    <div className="pt-[240px]">
+      <div className=" fixed top-33 left-0 right-0 z-[999] shadow-lg bg-[#f5f5f5]">
+        <div className=" flex justify-center items-center gap-20   py-[25px]  ">
+          <div className="flex justify-center items-center gap-5 font-popinse ">
+            {categoryProduct.map((cate) => (
+              <button
+                key={cate.value}
+                onClick={() => setActiveCategory(cate.value)}
+                className={` text-white px-[13px] py-[5px] rounded-[10px] cursor-pointer ${
+                  activeCategory === cate.value ? "bg-pink-300" : "bg-green-300"
+                }`}
+              >
+                {cate.label}
+              </button>
+            ))}
+          </div>
+          <div>
+            <input
+              onChange={handleSearch}
+              value={Searching}
+              className="border-1 border-white outline-white px-[10px] font-popinse text-[12px] bg-pink-200 text-white rounded-[10px] py-[10px] font-semibold  placeholder-white"
+              type="text"
+              name=""
+              placeholder="Search..."
+              id=""
+            />
+          </div>
         </div>
       </div>
       <section className="flex justify-center items-center">
         <section className="grid grid-cols-4 gap-10">
-          {filterCategory.map((prod) => (
+          {finalSearch.map((prod) => (
             <Link to={prod.id.toString()} key={prod.id}>
               {" "}
               <FeatureCard {...prod} />

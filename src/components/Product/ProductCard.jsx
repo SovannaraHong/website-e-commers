0f9e7641a39ab647f21ad987fb8b2b9c29c519ed
Hong Sovannara <rapewpew372@gmail.com>
2025-09-17 17:16:10 +0700

    update detail

--- src/components/Product/ProductCard.jsx
+++ src/components/Product/ProductCard.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const ProductCard = ({ id, productName, images, price }) => {
   const [mainImg, setMainimg] = useState(images[0].img);
@@ -20,13 +21,16 @@ const ProductCard = ({ id, productName, images, price }) => {
   return (
     <article className="w-[250px] rounded-[10px] shadow-lg cursor-pointer hover:scale-[1.05] transition-all duration-300">
       <div>
-        <img
-          onMouseEnter={() => images[3]?.img && setMainimg(images[3].img)}
-          onMouseLeave={() => setMainimg(images[0].img)}
-          className="object-cover h-[250px] w-[250px]"
-          src={mainImg}
-          alt={productName}
-        />
+        <Link to={id.toString()}>
+          {" "}
+          <img
+            onMouseEnter={() => images[3]?.img && setMainimg(images[3].img)}
+            onMouseLeave={() => setMainimg(images[0].img)}
+            className="object-cover h-[250px] w-[250px]"
+            src={mainImg}
+            alt={productName}
+          />
+        </Link>
       </div>
       <div className="flex justify-between items-center pb-[20px] pt-[5px] px-[15px]">
         <p className="font-semibold">{productName}</p>
